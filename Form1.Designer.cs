652d985318066c0e79d188b23daa5baed001facb
Alex Robot <nvalex825@yahoo.com>
2022-10-07 23:27:44 -0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Assignment_7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labName = new System.Windows.Forms.Label();
+            this.labLevel = new System.Windows.Forms.Label();
+            this.labReview = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.comReview = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnReferesh = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 18);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1035, 307);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // labName
+            // 
+            this.labName.AutoSize = true;
+            this.labName.Location = new System.Drawing.Point(36, 392);
+            this.labName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labName.Name = "labName";
+            this.labName.Size = new System.Drawing.Size(53, 19);
+            this.labName.TabIndex = 1;
+            this.labName.Text = "Name ";
+            this.labName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labLevel
+            // 
+            this.labLevel.AutoSize = true;
+            this.labLevel.Location = new System.Drawing.Point(36, 462);
+            this.labLevel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labLevel.Name = "labLevel";
+            this.labLevel.Size = new System.Drawing.Size(91, 19);
+            this.labLevel.TabIndex = 2;
+            this.labLevel.Text = "Level/Grade";
+            // 
+            // labReview
+            // 
+            this.labReview.AutoSize = true;
+            this.labReview.Location = new System.Drawing.Point(36, 531);
+            this.labReview.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labReview.Name = "labReview";
+            this.labReview.Size = new System.Drawing.Size(74, 19);
+            this.labReview.TabIndex = 3;
+            this.labReview.Text = "Reviewed";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(153, 392);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(193, 28);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLevel.Location = new System.Drawing.Point(153, 453);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(193, 28);
+            this.txtLevel.TabIndex = 5;
+            // 
+            // comReview
+            // 
+            this.comReview.FormattingEnabled = true;
+            this.comReview.Location = new System.Drawing.Point(162, 523);
+            this.comReview.Name = "comReview";
+            this.comReview.Size = new System.Drawing.Size(121, 27);
+            this.comReview.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(437, 355);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(99, 38);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(437, 422);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(99, 38);
+            this.btnSelect.TabIndex = 8;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(437, 493);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(99, 38);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnReferesh
+            // 
+            this.btnReferesh.Location = new System.Drawing.Point(577, 355);
+            this.btnReferesh.Name = "btnReferesh";
+            this.btnReferesh.Size = new System.Drawing.Size(99, 38);
+            this.btnReferesh.TabIndex = 10;
+            this.btnReferesh.Text = "Refresh";
+            this.btnReferesh.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(577, 422);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(99, 38);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(577, 493);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 38);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 658);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnReferesh);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.comReview);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labReview);
+            this.Controls.Add(this.labLevel);
+            this.Controls.Add(this.labName);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labName;
+        private System.Windows.Forms.Label labLevel;
+        private System.Windows.Forms.Label labReview;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.ComboBox comReview;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnReferesh;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassDeveloper;
+
+namespace Assignment_7_1
+{
+    public partial class Form1 : Form
+    {
+        List<DeveloperAttribute> devop;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            devop = new List<DeveloperAttribute>();
+            devop.Add(new DeveloperAttribute() { Name = "Jason", DeveloperGrade = "B", Review = false});
+            devop.Add(new DeveloperAttribute() { Name = "Smith", DeveloperGrade = "A", Review = true });
+            dataGridView1.DataSource = devop;
+            dataGridView1.Columns[3].Visible = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
